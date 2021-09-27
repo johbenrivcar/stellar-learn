9c5bef353830baa37320a81a9955f57c611e61be
JohnRC <38293004+johbenrivcar@users.noreply.github.com>
2021-09-27 16:34:13 +0100

    INITIAL COMMIT
    Basic set of git files

---
+++ CommandPrompt.bat
@@ -0,0 +1,5 @@
+color 01
+cd .
+title mpxwp
+start /D .  ENV
+
---
+++ ENV.bat
@@ -0,0 +1,28 @@
+@echo off
+REM TIGER APP - Stellar subset
+ECHO STELLAR SUBSET - //stellar
+title //stellar
+color 5F
+cd
+PROMPT stellar:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
